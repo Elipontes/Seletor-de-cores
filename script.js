07d54e9f4cfa1aeb49fab73a84563588570f19e9
Eli Pontes <heliarapontes@gmail.com>
2022-07-19 18:10:57 -0300

    mod

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function mudaCorFundo() {
     const color = menu.value;
     document.body.style.background = color;
     const texto = document.querySelector('#texto')
-    texto.innerHTML = 'Ótimo escolha! tente novamente!'
+    texto.innerHTML = 'Ótima escolha! tente novamente!'
 }
 
